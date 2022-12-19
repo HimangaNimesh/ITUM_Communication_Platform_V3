edcadab678eafeca7feccfb5cf6764361e397808
HimangaNimesh <himanganimesh2@gmail.com>
2022-12-19 16:47:44 +0530

    update 2 tabs and update comments

--- lib/pages/groups/chat_page.dart
+++ lib/pages/groups/chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:itum_communication_platform/notifications/inbox_notifications.dart';
 import 'package:itum_communication_platform/pages/polls/polls_home_page.dart';
 import 'package:itum_communication_platform/pages/polls/polls_view.dart';
@@ -50,139 +51,172 @@ class _ChatPageState extends State<ChatPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-        title: Text(widget.groupName, style: const TextStyle(color: Colors.black),),
-        backgroundColor: const Color(0xffE5F9FF),
-        leading: IconButton(
-            onPressed: (){
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.arrow_back_ios, color: Color(0xff649EFF))),
-        actions: [
-          IconButton(
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          title: Text(widget.groupName, style: const TextStyle(color: Colors.black),),
+          backgroundColor: const Color(0xffE5F9FF),
+          leading: IconButton(
               onPressed: (){
-                nextScreen(context, PollsViewPage());
+                Navigator.pop(context);
               },
-              icon: Icon(Icons.poll, color: Color(0xff649EFF),)),
-          IconButton(
-              onPressed: (){
-                nextScreen(context, InboxNotifications());
-              },
-              icon: Icon(Icons.notifications, color: Color(0xff649EFF),)),
-          Builder(
-            builder: (context) => IconButton(
+              icon: const Icon(Icons.arrow_back_ios, color: Color(0xff649EFF))),
+          actions: [
+            IconButton(
                 onPressed: (){
-                  Scaffold.of(context).openEndDrawer();
+                  nextScreen(context, PollsViewPage());
                 },
-                icon: const Icon(Icons.more_vert, color: Color(0xff649EFF),)),
-          ),
-        ],
-      ),
-        endDrawer: Drawer(
-          child: ListView(
-            padding: const EdgeInsets.symmetric(vertical: 50),
-            children: <Widget>[
-              ListTile(
-                onTap: (){
-                  nextScreen(context, GroupInfo(
-                    groupId: widget.groupId,
-                    groupName: widget.groupName,
-                    adminName: admin,
-                  ));
+                icon: Icon(Icons.poll, color: Color(0xff649EFF),)),
+            IconButton(
+                onPressed: (){
+                  nextScreen(context, InboxNotifications());
                 },
-                contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                leading: const Icon(Icons.info, color: Color(0xff649EFF),),
-                title: const Text('Group Info', style: TextStyle(color: Colors.black),),
-              ),
-              ListTile(
-                onTap: (){
-                  nextScreen(context, PollsHome(
-                  ));
-                },
-                contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                leading: const Icon(Icons.poll, color: Color(0xff649EFF),),
-                title: const Text('Create Polls', style: TextStyle(color: Colors.black),),
-              ),
-              ListTile(
-                onTap: (){
-                  nextScreen(context, RemindersHome(
-                  ));
-                },
-                contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                leading: const Icon(Icons.notifications, color: Color(0xff649EFF),),
-                title: const Text('Create Reminders', style: TextStyle(color: Colors.black),),
-              ),
-            ],
-          ),
-        ),
-      body: Stack(
-        children: <Widget>[
-          chatMessages(),
-          Container(
-            alignment: Alignment.bottomCenter,
-            width: MediaQuery.of(context).size.width,
-            child: Container(
-              margin: const EdgeInsets.all(10.0),
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20.0),
-                color: Colors.grey[700],
-              ),
-              width: MediaQuery.of(context).size.width,
-              child: Row(
-                children: [
-                  Expanded(
-                      child: TextFormField(
-                        controller: messageController,
-                        style: const TextStyle(color: Colors.white),
-                        decoration: const InputDecoration(
-                          hintText: "Send a message...",
-                          hintStyle: TextStyle(color: Colors.white),
-                          border: InputBorder.none,
-                        ),
-                      )),
-                  const SizedBox(width: 12,),
-                  GestureDetector(
-                    onTap: (){
-                      sendMessage();
-                    },
-                    child: Container(
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                        color: const Color(0xff649EFF),
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: const Center(
-                        child: Icon(Icons.send, color: Colors.white,),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                icon: Icon(Icons.notifications, color: Color(0xff649EFF),)),
+            Builder(
+              builder: (context) => IconButton(
+                  onPressed: (){
+                    Scaffold.of(context).openEndDrawer();
+                  },
+                  icon: const Icon(Icons.more_vert, color: Color(0xff649EFF),)),
             ),
-          )
-        ],
-      )
+          ],
+          bottom: TabBar(
+            labelColor: Colors.black,
+              tabs: [
+                Tab(
+                  text: 'Students',
+                ),
+                Tab(
+                  text: 'Admin',
+                )
+              ]),
+        ),
+          endDrawer: Drawer(
+            child: ListView(
+              padding: const EdgeInsets.symmetric(vertical: 50),
+              children: <Widget>[
+                ListTile(
+                  onTap: (){
+                    nextScreen(context, GroupInfo(
+                      groupId: widget.groupId,
+                      groupName: widget.groupName,
+                      adminName: admin,
+                    ));
+                  },
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  leading: const Icon(Icons.info, color: Color(0xff649EFF),),
+                  title: const Text('Group Info', style: TextStyle(color: Colors.black),),
+                ),
+                ListTile(
+                  onTap: (){
+                    nextScreen(context, PollsHome(
+                    ));
+                  },
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  leading: const Icon(Icons.poll, color: Color(0xff649EFF),),
+                  title: const Text('Create Polls', style: TextStyle(color: Colors.black),),
+                ),
+                ListTile(
+                  onTap: (){
+                    nextScreen(context, RemindersHome(
+                    ));
+                  },
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  leading: const Icon(Icons.notifications, color: Color(0xff649EFF),),
+                  title: const Text('Create Reminders', style: TextStyle(color: Colors.black),),
+                ),
+              ],
+            ),
+          ),
+        body: Column(
+          children: [
+
+            Expanded(
+              child: TabBarView(
+                  children: [
+                    Container(
+                      child: Stack(
+                        children: <Widget>[
+                          chatMessages(),
+                          Container(
+                            alignment: Alignment.bottomCenter,
+                            width: MediaQuery.of(context).size.width,
+                            child: Container(
+                              margin: const EdgeInsets.all(10.0),
+                              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20.0),
+                                color: Colors.grey[700],
+                              ),
+                              width: MediaQuery.of(context).size.width,
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                      child: TextFormField(
+                                        controller: messageController,
+                                        style: const TextStyle(color: Colors.white),
+                                        decoration: const InputDecoration(
+                                          hintText: "Send a message...",
+                                          hintStyle: TextStyle(color: Colors.white),
+                                          border: InputBorder.none,
+                                        ),
+                                      )),
+                                  const SizedBox(width: 12,),
+                                  GestureDetector(
+                                    onTap: (){
+                                      sendMessage();
+                                    },
+                                    child: Container(
+                                      height: 50,
+                                      width: 50,
+                                      decoration: BoxDecoration(
+                                        color: const Color(0xff649EFF),
+                                        borderRadius: BorderRadius.circular(30),
+                                      ),
+                                      child: const Center(
+                                        child: Icon(Icons.send, color: Colors.white,),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ),
+                    Container(
+                      child: Center(
+                        child: Text('Admin'),
+                      )
+                    )
+                  ]),
+            )
+          ],
+        )
+
+      ),
     );
   }
   chatMessages(){
     return StreamBuilder(
-      stream: chats,
+        stream: chats,
         builder: (context,AsyncSnapshot snapshot){
-        return snapshot.hasData ?
-        ListView.builder(
-          itemCount: snapshot.data.docs.length,
-            itemBuilder: (context, index){
-            return MessageTile(
-                message: snapshot.data.docs[index]['message'],
-                sender: snapshot.data.docs[index]['sender'],
-                sentByMe: widget.userName == snapshot.data.docs[index]['sender']);
-            })
-            : Container();
+          return snapshot.hasData ?
+          ListView.builder(
+
+              itemCount: snapshot.data.docs.length,
+              itemBuilder: (context, index){
+                return MessageTile(
+                    message: snapshot.data.docs[index]['message'],
+                    sender: snapshot.data.docs[index]['sender'],
+                    sentByMe: widget.userName == snapshot.data.docs[index]['sender']);
+              },
+          )
+              : Container();
         });
   }
 
